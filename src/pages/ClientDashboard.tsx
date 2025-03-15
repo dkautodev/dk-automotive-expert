ae8d045f0458ff641c3a0c93840fdca11cc25595
dkautodev <dkautomotive70@gmail.com>
2025-03-15 15:52:21 +0000

    Navigate to order page
    Navigates to the order page after submitting the form, carrying over the entered information.

--- src/pages/ClientDashboard.tsx
+++ src/pages/ClientDashboard.tsx
@@ -5,6 +5,7 @@ import { ClientSidebar } from "@/components/client/ClientSidebar";
 import DashboardHome from "@/components/client/DashboardHome";
 import Profile from "@/components/client/Profile";
 import OrderHistory from "@/components/client/OrderHistory";
+import OrderDetails from "./OrderDetails";
 
 const ClientDashboard = () => {
   return (
@@ -16,6 +17,7 @@ const ClientDashboard = () => {
             <Route index element={<DashboardHome />} />
             <Route path="profile" element={<Profile />} />
             <Route path="orders" element={<OrderHistory />} />
+            <Route path="order-details" element={<OrderDetails />} />
             <Route path="*" element={<Navigate to="/dashboard/client" replace />} />
           </Routes>
         </main>
