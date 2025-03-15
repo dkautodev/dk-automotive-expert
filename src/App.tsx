a76f7f0fc0f9e29a3e83e82886b2f3f5dfca9e3e
dkautodev <dkautomotive70@gmail.com>
2025-03-15 13:23:32 +0000

    Create FAQ page
    Create a FAQ page with the same style and questions/answers as the provided URL, including the menu bar and footer.

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import Devis from "./pages/Devis";
 import Contact from "./pages/Contact";
+import FAQ from "./pages/FAQ";
 import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
@@ -22,7 +23,7 @@ const App = () => (
           <Route path="/devis" element={<Devis />} />
           <Route path="/contact" element={<Contact />} />
           <Route path="/about" element={<NotFound />} />
-          <Route path="/faq" element={<NotFound />} />
+          <Route path="/faq" element={<FAQ />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
