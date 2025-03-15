f913c14b0478cbe1f20b9ed2c841574050d95cc4
dkautodev <dkautomotive70@gmail.com>
2025-03-15 14:37:46 +0000

    Fix page navigation issue
    Ensure pages start at the top when navigating between links.

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { ScrollToTop } from "./components/ScrollToTop";
 import Index from "./pages/Index";
 import About from "./pages/About";
 import Devis from "./pages/Devis";
@@ -22,6 +22,7 @@ const App = () => (
       <Toaster />
       <Sonner />
       <BrowserRouter>
+        <ScrollToTop />
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/devis" element={<Devis />} />
