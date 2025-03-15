fb294be50d0047a113bc98a9ab1576dbcf65208b
dkautodev <dkautomotive70@gmail.com>
2025-03-15 16:41:00 +0000

    Add vehicle registration and quote button
    - Add a required field for vehicle registration.
    - Add a "Get Your Quote" button that redirects to the "Total Quote" page when at least one vehicle is added.

---
+++ src/pages/QuoteTotal.tsx
@@ -0,0 +1,20 @@
+
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+
+const QuoteTotal = () => {
+  return (
+    <div className="p-6">
+      <Card>
+        <CardHeader>
+          <CardTitle>Total de votre devis</CardTitle>
+        </CardHeader>
+        <CardContent>
+          {/* Le contenu du devis sera implémenté plus tard */}
+          <p>Le détail de votre devis sera affiché ici.</p>
+        </CardContent>
+      </Card>
+    </div>
+  );
+};
+
+export default QuoteTotal;
