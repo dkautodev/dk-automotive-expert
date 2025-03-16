c027444d833e603c658f9931384928d8abe121d9
dkautodev <dkautomotive70@gmail.com>
2025-03-16 18:27:11 +0000

    Update Google Maps API key
    Updates the Google Maps API key in constants.ts.

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,4 +1,4 @@
 
 // Assurez-vous que la clé ne contient pas d'espaces avant ou après
-export const GOOGLE_MAPS_API_KEY = 'AIzaSyCM8tDTL3wq3_oK_0rMGtR_fqWqk5hWgp4'.trim();
+export const GOOGLE_MAPS_API_KEY = 'AIzaSyAq7jjZ09lIT6OqVe4BwNtX4QPwr3xSKPU'.trim();
 
